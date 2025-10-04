fc793f7314c6498fc153fafb5f8aff224d80af83
woisy00 <Andreas@Bergermeier.com>
2025-10-04 19:17:33 +0200

    Eta device (#1)
    * device setup
    
    * eta pellets unit as device
    
    ---------
    
    Co-authored-by: Bergermeier Andreas <bean@tgw-group.com>

--- custom_components/eta/api.py
+++ custom_components/eta/api.py
@@ -1,4 +1,149 @@
+from enum import Enum
+
 import xmltodict
+import asyncio
+from typing import Sequence
+from homeassistant.helpers.selector import SelectOptionDict
+class SensorType(Enum):
+    NUMERIC = "numeric"
+    TEXT = "text"
+
+
+FLOAT_SENSOR_UNITS = [
+    "%",
+    "A",
+    "Hz",
+    "Ohm",
+    "Pa",
+    "U/min",
+    "V",
+    "W",
+    "W/m²",
+    "bar",
+    "kW",
+    "kWh",
+    "kg",
+    "l",
+    "l/min",
+    "mV",
+    "m²",
+    "s",
+    "°C",
+    "%rH",
+]
+
+
+class EtaSensorDesc:
+    def __init__(self, id, name, parent, sensor_type=SensorType.NUMERIC):
+        self._id = id
+        self._name = name
+        self._parent = parent
+        self._unit = None
+        self._sensor_type = sensor_type
+        self._states = None  # for text sensors, possible states
+        self._canonicalName = None
+
+    def updateName(self, canonicalName):
+        self._canonicalName = canonicalName
+
+    def updateUnit(self, unit):
+        self._unit = unit
+        self._sensor_type = SensorType.NUMERIC
+
+    def updateStates(self, states: dict):
+        self._states = states
+        self._sensor_type = SensorType.TEXT
+
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def unit(self):   
+        return self._unit
+
+    @property
+    def sensor_type(self):
+        return self._sensor_type
+    
+    def getValue(self, data) -> float | str:
+        match self._sensor_type:
+            case SensorType.TEXT:
+                value = data["#text"]
+                return self._states.get(value, data["@strValue"])
+            case SensorType.NUMERIC:
+                unit = data["@unit"]
+                if unit in FLOAT_SENSOR_UNITS:
+                    scale_factor = int(data["@scaleFactor"])
+                    decimal_places = int(data["@decPlaces"])
+                    raw_value = float(data["#text"])
+                    value = raw_value / scale_factor
+                    value = round(value, decimal_places)
+                else:
+                    # use default text string representation for values that cannot be parsed properly
+                    value = data["@strValue"]
+                return value
+
+        return -1
+
+    def map(self, value):
+        if self._states:
+            return self._states.get(value, value)
+        else:
+            return value
+
+    def canonicalName(self) -> str:
+        if self._canonicalName:
+            return self._canonicalName
+        else:
+            cn = ""
+            if self._parent:
+                cn = cn + self._parent.canonicalName() + " > "
+
+            cn = cn + self._name
+            return cn
+
+
+class SensorDict:
+    def __init__(self) -> None:
+        # id to sensor
+        self._sensors: dict[str, EtaSensorDesc] = {}
+        self._label_to_id: dict[str, str] = {}
+        self._id_to_label: dict[str, str] = {}
+
+    def add(self, sensor: EtaSensorDesc):
+        if not sensor.id in self._sensors:
+            self._sensors[sensor.id] = sensor
+            self._id_to_label[sensor.id] = sensor.canonicalName()
+            self._label_to_id[sensor.canonicalName()] = sensor.id
+
+    def update(self, sensor: EtaSensorDesc):
+        oldLabel = self._id_to_label[sensor.id]
+        self._id_to_label[sensor.id] = sensor.canonicalName()
+        self._label_to_id[sensor.canonicalName()] = sensor.id
+        self._label_to_id.pop(oldLabel)
+
+    @property
+    def sensors(self) -> dict[str, EtaSensorDesc]:
+        return self._sensors
+
+    def byId(self, id: str) -> EtaSensorDesc:
+        return self._sensors[id]
+
+    def byName(self, name: str) -> EtaSensorDesc:
+        id = self._label_to_id[name]
+        return self._sensors[id]
+
+    def names(self) -> list[str]:
+        return list(self._label_to_id.keys())
+    
+    def nameDict(self) -> list[dict[str, str]]:
+        options = [{"value": k, "label": v} for k, v in self._id_to_label.items()]
+        return options
 
 
 class EtaAPI:
@@ -6,97 +151,105 @@ class EtaAPI:
         self._session = session
         self._host = host
         self._port = port
+        self._initialized = False
+        self._sensors = SensorDict()
 
-        self._float_sensor_units = [
-            "%",
-            "A",
-            "Hz",
-            "Ohm",
-            "Pa",
-            "U/min",
-            "V",
-            "W",
-            "W/m²",
-            "bar",
-            "kW",
-            "kWh",
-            "kg",
-            "l",
-            "l/min",
-            "mV",
-            "m²",
-            "s",
-            "°C",
-            "%rH"
-        ]
-
-    def build_uri(self, suffix):
+    def _build_uri(self, suffix):
         return "http://" + self._host + ":" + str(self._port) + suffix
 
-    def evaluate_xml_dict(self, xml_dict, uri_dict, prefix=""):
-        if type(xml_dict) == list:
+    def _evaluate_xml_dict(self, xml_dict, parent):
+        if type(xml_dict) is list:
             for child in xml_dict:
-                self.evaluate_xml_dict(child, uri_dict, prefix)
+                self._evaluate_xml_dict(child, parent)
         else:
             if "object" in xml_dict:
                 child = xml_dict["object"]
-                new_prefix = f"{prefix}_{xml_dict['@name']}"
+                id = xml_dict["@uri"]
+                s = EtaSensorDesc(id, xml_dict["@name"], parent)
                 # add parent to uri_dict and evaluate childs then
-                uri_dict[f"{prefix}_{xml_dict['@name']}"] = xml_dict["@uri"]
-                self.evaluate_xml_dict(child, uri_dict, new_prefix)
+                self._sensors.add(s)
+                self._evaluate_xml_dict(child, s)
+            elif "fub" in xml_dict:
+                fub = xml_dict["fub"]
+                id = xml_dict["@uri"]
+                s = EtaSensorDesc(id, xml_dict["@name"], None)
+                # add parent to uri_dict and evaluate childs then
+                self._sensors.add(s)
+                self._evaluate_xml_dict(fub, s)
             else:
-                uri_dict[f"{prefix}_{xml_dict['@name']}"] = xml_dict["@uri"]
+                id = xml_dict["@uri"]
+                s = EtaSensorDesc(id, xml_dict["@name"], parent)
+                # add parent to uri_dict and evaluate childs then
+                self._sensors.add(s)
 
-    async def get_request(self, suffix):
-        data = await self._session.get(self.build_uri(suffix))
+    async def _get_request(self, suffix):
+        data = await self._session.get(self._build_uri(suffix))
         return data
 
-    async def does_endpoint_exists(self):
-        resp = await self.get_request("/user/menu")
-        return resp.status == 200
-
-    async def _parse_data(self, data):
-        unit = data["@unit"]
-        if unit in self._float_sensor_units:
-            scale_factor = int(data["@scaleFactor"])
-            decimal_places = int(data["@decPlaces"])
-            raw_value = float(data["#text"])
-            value = raw_value / scale_factor
-            value = round(value, decimal_places)
-        else:
-            # use default text string representation for values that cannot be parsed properly
-            value = data["@strValue"]
-        return value, unit
-
-    async def get_data(self, uri):
-        data = await self.get_request("/user/var/" + str(uri))
+    async def get_data(self, sensor: EtaSensorDesc):
+        data = await self._get_request("/user/var" + sensor.id)
         text = await data.text()
         data = xmltodict.parse(text)["eta"]["value"]
-        return await self._parse_data(data)
+        return sensor.getValue(data)
 
-    async def get_raw_sensor_dict(self):
-        data = await self.get_request("/user/menu/")
+    async def initializeSensor(self, sensor: EtaSensorDesc):
+        try:
+            data = await self._get_request("/user/varinfo" + sensor.id)
+            text = await data.text()
+            xml = xmltodict.parse(text)
+            varInfo = xml["eta"].get("varInfo", None)
+            if varInfo:
+                type = varInfo["variable"]["type"]
+                name = varInfo["variable"]["@fullName"]
+                if name:
+                    sensor.updateName(name)
+                    # name (canonical has changed, add to dict again)
+                    self._sensors.add(sensor)
+
+                match type:
+                    case "TEXT":
+                        states = {}
+                        for v in varInfo["variable"]["validValues"]["value"]:
+                            states[v["#text"]] = v["@strValue"]
+                        sensor.updateStates(states)
+                    case "DEFAULT":
+                        unit = varInfo["variable"]["@unit"]
+                        if unit in FLOAT_SENSOR_UNITS:
+                            sensor.updateUnit(unit)
+                    case "TIMESLOT":
+                        pass
+                    case _:
+                        print(f"Unknown ETA Sensor Type: {type}")
+        except Exception as e:
+            print(f"Failed to update sensor definition {sensor.id}: {e}")
+
+    async def _get_raw_sensor_dict(self):
+        data = await self._get_request("/user/menu/")
         text = await data.text()
         data = xmltodict.parse(text)
         raw_dict = data["eta"]["menu"]["fub"]
         return raw_dict
 
-    async def get_sensors_dict(self):
-        raw_dict = await self.get_raw_sensor_dict()
-        uri_dict = {}
-        self.evaluate_xml_dict(raw_dict, uri_dict)
-        return uri_dict
+    async def _initialize(self):
+        if not self._initialized:
+            raw_dict = await self._get_raw_sensor_dict()
+            self._evaluate_xml_dict(raw_dict, None)
+            self._initialized = True
 
-    async def get_float_sensors(self):
-        sensor_dict = await self.get_sensors_dict()
-        float_dict = {}
-        for key in sensor_dict:
-            try:
-                value, unit = await self.get_data(sensor_dict[key])
-                if unit not in self._float_sensor_units:
-                    continue
-                cleaned_key = key.lower().replace(" ", "_")
-                float_dict[cleaned_key] = (sensor_dict[key], value, unit)
-            except:
-                pass
-        return float_dict
+
+    async def get_sensors(self) -> SensorDict:
+        await self._initialize()
+        return self._sensors
+
+
+class EtaAPIFactory:
+    """Factory to manage and cache EtaAPI instances."""
+
+    _instances = {}
+
+    @staticmethod
+    def get_instance(session, host, port) -> EtaAPI:
+        key = (host, port)
+        if key not in EtaAPIFactory._instances:
+            EtaAPIFactory._instances[key] = EtaAPI(session, host, port)
+        return EtaAPIFactory._instances[key]
--- custom_components/eta/sensor.py
+++ custom_components/eta/sensor.py
@@ -1,13 +1,5 @@
 """
 Platform for ETA sensor integration in Home Assistant
-
-Help Links:
- Entity Source: https://github.com/home-assistant/core/blob/dev/homeassistant/helpers/entity.py
- SensorEntity derives from Entity https://github.com/home-assistant/core/blob/dev/homeassistant/components/sensor/__init__.py
-
-
-author nigl
-
 """
 
 from __future__ import annotations
@@ -15,8 +7,9 @@ from __future__ import annotations
 import logging
 from datetime import timedelta
 
-_LOGGER = logging.getLogger(__name__)
-from .api import EtaAPI
+from voluptuous import Switch
+
+from .api import EtaAPI, EtaAPIFactory, SensorDict
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
@@ -29,9 +22,14 @@ from homeassistant.core import HomeAssistant
 from homeassistant import config_entries
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.entity import generate_entity_id
-from homeassistant.const import CONF_HOST, CONF_PORT
-from .const import DOMAIN, CHOOSEN_ENTITIES, FLOAT_DICT
 
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_NAME, CONF_MODEL
+from .const import DOMAIN, CHOOSEN_ENTITIES, FLOAT_DICT
+from .api import SensorType, EtaSensorDesc
+
+from homeassistant.helpers.device_registry import DeviceEntryType
+
+_LOGGER = logging.getLogger(__name__)
 SCAN_INTERVAL = timedelta(minutes=1)
 
 
@@ -40,75 +38,90 @@ async def async_setup_entry(
     config_entry: config_entries.ConfigEntry,
     async_add_entities,
 ):
-    """Setup sensors from a config entry created in the integrations UI."""
-    config = hass.data[DOMAIN][config_entry.entry_id]
-    # Update our config to include new repos and remove those that have been removed.
-    if config_entry.options:
-        config.update(config_entry.options)
+    """Set up ETA Device and sensors from config entry."""
+    config = config_entry.data
+    sensors = []
 
-    choosen_entities = config[CHOOSEN_ENTITIES]
-    sensors = [
-        EtaSensor(
-            config,
-            hass,
-            entity,
-            config[FLOAT_DICT][entity][0],
-            config[FLOAT_DICT][entity][2],
+    # Device info for the ETA Device
+    device_info = {
+        "identifiers": {(DOMAIN, f"{config[CONF_HOST]}:{config[CONF_PORT]}")},
+        "name": f"{config[CONF_NAME]}",
+        "manufacturer": "ETA Heiztechnik GmbH",
+        "model": f"{config[CONF_MODEL]}",
+        #"entry_type": DeviceEntryType.SERVICE,
+        "configuration_url": f"http://{config[CONF_HOST]}:{config[CONF_PORT]}/user/menu/"
+    }
+
+    # Create a single EtaAPI instance to share
+    session = async_get_clientsession(hass)
+    eta_api = EtaAPIFactory.get_instance(session, config[CONF_HOST], config[CONF_PORT])
+    sensors_dict = await eta_api.get_sensors()
+
+    # Add sensors for each selected entity
+    for entity in config.get(CHOOSEN_ENTITIES, []):
+        s = sensors_dict.byId(entity)
+        sensors.append(
+            EtaSensor(
+                name=s.name,
+                sensor=s,
+                eta_api=eta_api,
+                device_info=device_info,
+                hass=hass
+            )
         )
-        for entity in choosen_entities
-    ]
+
     async_add_entities(sensors, update_before_add=True)
 
 
 class EtaSensor(SensorEntity):
-    """Representation of a Sensor."""
+    """Representation of an ETA Sensor."""
 
-    def __init__(
-        self, config, hass, name, uri, unit, state_class=SensorStateClass.MEASUREMENT
-    ):
-        """
-        Initialize sensor.
+    def __init__(self, name, sensor: EtaSensorDesc, eta_api: EtaAPI, device_info, hass: HomeAssistant):
+        self._attr_name = f"{device_info["name"]} {name}"
+        self.entity_id = generate_entity_id(ENTITY_ID_FORMAT, "eta_" + sensor.canonicalName().replace(" > ", "_"), hass=hass)
+        self._sensor = sensor
+        self._eta_api = eta_api
+        self._device_info = device_info
+        self._attr_unique_id = f"eta_{self._eta_api._host}_{self._eta_api._port}_{sensor.id}"
+        self._value = None
+        self._initialized = False
 
-        To show all values: http://192.168.178.75:8080/user/menu
+    @property
+    def device_info(self):
+        return self._device_info
 
-        There are:
-          - entity_id - used to reference id, english, e.g. "eta_outside_temperature"
-          - name - Friendly name, e.g "Außentemperatur" in local language
+    @property
+    def native_value(self):
+        return self._sensor.map(self._value)
 
-        """
-        _LOGGER.warning("ETA Integration - init sensor")
+    @property
+    def extra_state_attributes(self):
+        return {
+            "sensor_id": self._sensor.id
+        }    
 
-        self._attr_device_class = self.determine_device_class(unit)
-
-
-        if unit == "":
-            unit = None
-
-        if self._attr_device_class == SensorDeviceClass.ENERGY:
-            self._attr_state_class = SensorStateClass.TOTAL_INCREASING
-        else:
-            self._attr_state_class = state_class
-
-        self._attr_native_unit_of_measurement = unit
-        self._attr_native_value = float
-        id = name.lower().replace(" ", "_")
-        self._attr_name = name  # friendly name - local language
-        self.entity_id = generate_entity_id(ENTITY_ID_FORMAT, "eta_" + id, hass=hass)
-        self.session = async_get_clientsession(hass)
-
-        self.uri = uri
-        self.host = config.get(CONF_HOST)
-        self.port = config.get(CONF_PORT)
-        self._attr_unique_id = f"eta_{self.host}_{name.replace(' ', '_')}"
-
+    async def initialize(self):
+        """Initialize sensor."""
+        if not self._initialized:
+            await self._eta_api.initializeSensor(self._sensor)
+            self._attr_native_unit_of_measurement = self._sensor.unit
+            self._attr_device_class = self.determine_device_class(self._sensor.unit)
+            
+            self._initialized = True
+    
     async def async_update(self):
-        """Fetch new state data for the sensor.
-        This is the only method that should fetch new data for Home Assistant.
-        readme: activate first: http://www.holzheizer-forum.de/attachment/28434-eta-restful-v1-1-pdf/
-        """
-        eta_client = EtaAPI(self.session, self.host, self.port)
-        value, _ = await eta_client.get_data(self.uri)
-        self._attr_native_value = float(value)
+        """Fetch new state data for the sensor."""
+        try:
+            await self.initialize()
+            match self._sensor.sensor_type:
+                case SensorType.TEXT:
+                    value = await self._eta_api.get_data(self._sensor)
+                    self._value = value
+                case SensorType.NUMERIC:
+                    value = await self._eta_api.get_data(self._sensor)
+                    self._value = float(value)
+        except Exception as e:
+            _LOGGER.warning(f"Failed to update ETA sensor {self._attr_name}: {e}")
 
     @staticmethod
     def determine_device_class(unit):
